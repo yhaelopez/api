a45d20cf6f6bc688e5eb8bab908b2c09e64e5421
Yhael S <yhael@aloware.com>
2025-04-22 17:33:58 -0600

    everything working good except for SSL, missing configuration is missing

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: process.env.VITE_HMR_HOST || 'localhost', // Use environment variable or default
+            protocol: 'ws', // Use WebSocket protocol
+            port: process.env.VITE_HMR_PORT || 5173, // Use environment variable or default port
+        },
+    },
+});
