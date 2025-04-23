6e06f03749fe29096950aebf754cd08a75a63c2c
Yhael S <yhael@aloware.com>
2025-04-23 00:08:03 -0600

    added swagger comments

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,8 +14,8 @@ use OpenApi\Annotations as OA;
  *     @OA\Property(property="id", type="integer", example=1),
  *     @OA\Property(property="name", type="string", example="John Doe"),
  *     @OA\Property(property="email", type="string", example="john.doe@example.com"),
- *     @OA\Property(property="created_at", type="string", format="date-time", example="2021-01-01 12:00:00"),
- *     @OA\Property(property="updated_at", type="string", format="date-time", example="2021-01-01 12:00:00")
+ *     @OA\Property(property="created_at", type="datetime", example="2021-01-01 12:00:00"),
+ *     @OA\Property(property="updated_at", type="datetime", example="2021-01-01 12:00:00")
  * )
  */
 class UserResource extends JsonResource
